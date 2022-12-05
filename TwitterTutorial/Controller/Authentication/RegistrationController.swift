c0668f84ef4f527e14c14f4c687c456ee79ed598
Guowei Lv <lvguowei1002@gmail.com>
2022-12-05 22:59:59 +0200

    Getting started with Login UI

---
+++ TwitterTutorial/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,28 @@
+//
+//  RegistrationController.swift
+//  TwitterTutorial
+//
+//  Created by Guowei Lv on 5.12.2022.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    // MARK: - Properties
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    // MARK: - Selectors
+
+    // MARK: - Helpers
+
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+
+}
