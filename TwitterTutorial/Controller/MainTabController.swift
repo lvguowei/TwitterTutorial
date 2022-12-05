e1886906336cf811eacc8440839bdb3b1c0e1152
Guowei Lv <lvguowei1002@gmail.com>
2022-12-05 11:30:39 +0200

    Setting up sticky action button programatically

--- TwitterTutorial/Controller/MainTabController.swift
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -4,15 +4,33 @@ class MainTabController: UITabBarController {
 
     // MARK: - Properties
 
+    let actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.backgroundColor = .blue
+        button.setImage(UIImage(named: "new_tweet"), for: .normal)
+        return button
+    }()
+
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewControllers()
+        configureUI()
         tabBar.backgroundColor = .white
     }
 
     // MARK: - Helpers
+
+    func configureUI() {
+        view.addSubview(actionButton)
+        actionButton.anchor(
+            bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor,
+            paddingBottom: 64, paddingRight: 16,
+            width: 56, height: 56)
+        actionButton.layer.cornerRadius = 56 / 2
+    }
     func configureViewControllers() {
         let feed = FeedController()
         let nav1 = templateNavigationController(
