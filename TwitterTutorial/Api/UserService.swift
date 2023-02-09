2e32e33c4002f2dde05183bb1d40e601eb9e792d
Guowei Lv <lvguowei1002@gmail.com>
2023-02-09 14:39:07 +0200

    22 Fetch User Data from Firebase

--- TwitterTutorial/Api/UserService.swift
+++ TwitterTutorial/Api/UserService.swift
@@ -15,7 +15,6 @@ struct UserService {
         REF_USERS.child(uid).observeSingleEvent(of: .value) { snapshot in
             guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
             let user = User(uid: uid, dictionary: dictionary)
-            print("DEBUG: \(user)")
         }
     }
 }
